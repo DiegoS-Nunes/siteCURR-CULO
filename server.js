746f66ef70a94505825adac38f8582426d4f60d2
DiegoS-Nunes <DiegoS-Nunes@users.noreply.github.com>
2024-06-17 00:30:15 +0000

    deploy: 19dd502120dbe066e0cdc7a69bc200f5672ca74d

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express'
+import { router } from './routes.js';
+import { openDb } from './db/configDB.js';
 
 const server = express();
 const PORT = 3000;
-
-server.get('/', async (req, res) => {
-    return res.send('Hello World!');
-});
+server.use(express.json());
+server.use(router);
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
